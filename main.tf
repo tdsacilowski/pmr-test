c551dabf845f3300535b7c5b1aefe891c617753e
tdsacilowski <tdsacilowski@users.noreply.github.com>
2018-01-09 11:15:40 -0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
 //--------------------------------------------------------------------
-// Variables
-variable "rds_password" {}
-
-//--------------------------------------------------------------------
 // Modules
-module "rds" {
-  source  = "teddy-ptfe.hashidemos.io/teddy-ptfe-demo/rds/aws"
-  version = "1.6.0"
+module "vpc" {
+  source  = "teddy-ptfe.hashidemos.io/teddy-ptfe-demo/vpc/aws"
+  version = "1.2.0"
 
-  allocated_storage = 5
-  backup_window = "03:00-06:00"
-  engine = "mysql"
-  engine_version = "5.7.19"
-  identifier = "pmr-test-rds"
-  instance_class = "db.t2.large"
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  name = "testdb"
-  password = "${var.rds_password}"
-  port = 3306
-  username = "admin"
+  cidr = "10.11.0.0/16"
+  create_database_subnet_group = "false"
+  name = "teddy-pmr-test"
 }
